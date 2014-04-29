23972ade649353aec17360123a84ba9b13bc7346
apkami <ap.corrupt.nirv@hotmail.com>
2014-04-29 23:14:28 +0930

    topics project

---
+++ PartyLights/PartyLights/MainWindow.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PartyLights
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelSensorValue = new System.Windows.Forms.Label();
+            this.labelPartyMode = new System.Windows.Forms.Label();
+            this.lblSensorValue = new System.Windows.Forms.Label();
+            this.lblPartyMode = new System.Windows.Forms.Label();
+            this.tmrMotion = new System.Windows.Forms.Timer(this.components);
+            this.tmrTick = new System.Windows.Forms.Timer(this.components);
+            this.labelCO2 = new System.Windows.Forms.Label();
+            this.labelTemp = new System.Windows.Forms.Label();
+            this.labelHumid = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblCO2 = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblHumidity = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCOM = new System.Windows.Forms.ComboBox();
+            this.tmrRefresh = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelSensorValue
+            // 
+            this.labelSensorValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSensorValue.Location = new System.Drawing.Point(26, 71);
+            this.labelSensorValue.Name = "labelSensorValue";
+            this.labelSensorValue.Size = new System.Drawing.Size(115, 42);
+            this.labelSensorValue.TabIndex = 0;
+            this.labelSensorValue.Text = "Motion Sensor Value";
+            // 
+            // labelPartyMode
+            // 
+            this.labelPartyMode.AutoSize = true;
+            this.labelPartyMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPartyMode.Location = new System.Drawing.Point(26, 24);
+            this.labelPartyMode.Name = "labelPartyMode";
+            this.labelPartyMode.Size = new System.Drawing.Size(84, 18);
+            this.labelPartyMode.TabIndex = 1;
+            this.labelPartyMode.Text = "Party Mode";
+            // 
+            // lblSensorValue
+            // 
+            this.lblSensorValue.AutoSize = true;
+            this.lblSensorValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSensorValue.Location = new System.Drawing.Point(203, 71);
+            this.lblSensorValue.Name = "lblSensorValue";
+            this.lblSensorValue.Size = new System.Drawing.Size(16, 18);
+            this.lblSensorValue.TabIndex = 2;
+            this.lblSensorValue.Text = "0";
+            // 
+            // lblPartyMode
+            // 
+            this.lblPartyMode.AutoSize = true;
+            this.lblPartyMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPartyMode.Location = new System.Drawing.Point(203, 24);
+            this.lblPartyMode.Name = "lblPartyMode";
+            this.lblPartyMode.Size = new System.Drawing.Size(38, 18);
+            this.lblPartyMode.TabIndex = 3;
+            this.lblPartyMode.Text = "OFF";
+            // 
+            // tmrMotion
+            // 
+            this.tmrMotion.Interval = 5000;
+            this.tmrMotion.Tick += new System.EventHandler(this.tmrMotion_Tick);
+            // 
+            // tmrTick
+            // 
+            this.tmrTick.Enabled = true;
+            this.tmrTick.Interval = 40;
+            this.tmrTick.Tick += new System.EventHandler(this.tmrTick_Tick);
+            // 
+            // labelCO2
+            // 
+            this.labelCO2.AutoSize = true;
+            this.labelCO2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCO2.Location = new System.Drawing.Point(26, 179);
+            this.labelCO2.Name = "labelCO2";
+            this.labelCO2.Size = new System.Drawing.Size(43, 18);
+            this.labelCO2.TabIndex = 5;
+            this.labelCO2.Text = "CO2:";
+            // 
+            // labelTemp
+            // 
+            this.labelTemp.AutoSize = true;
+            this.labelTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTemp.Location = new System.Drawing.Point(26, 220);
+            this.labelTemp.Name = "labelTemp";
+            this.labelTemp.Size = new System.Drawing.Size(96, 18);
+            this.labelTemp.TabIndex = 6;
+            this.labelTemp.Text = "Temperature:";
+            // 
+            // labelHumid
+            // 
+            this.labelHumid.AutoSize = true;
+            this.labelHumid.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHumid.Location = new System.Drawing.Point(26, 261);
+            this.labelHumid.Name = "labelHumid";
+            this.labelHumid.Size = new System.Drawing.Size(69, 18);
+            this.labelHumid.TabIndex = 7;
+            this.labelHumid.Text = "Humidity:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 347);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(276, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblCO2
+            // 
+            this.lblCO2.AutoSize = true;
+            this.lblCO2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCO2.Location = new System.Drawing.Point(203, 179);
+            this.lblCO2.Name = "lblCO2";
+            this.lblCO2.Size = new System.Drawing.Size(16, 18);
+            this.lblCO2.TabIndex = 9;
+            this.lblCO2.Text = "0";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.Location = new System.Drawing.Point(203, 220);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(16, 18);
+            this.lblTemp.TabIndex = 10;
+            this.lblTemp.Text = "0";
+            // 
+            // lblHumidity
+            // 
+            this.lblHumidity.AutoSize = true;
+            this.lblHumidity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHumidity.Location = new System.Drawing.Point(203, 261);
+            this.lblHumidity.Name = "lblHumidity";
+            this.lblHumidity.Size = new System.Drawing.Size(16, 18);
+            this.lblHumidity.TabIndex = 11;
+            this.lblHumidity.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Serial Port:";
+            // 
+            // cmbCOM
+            // 
+            this.cmbCOM.FormattingEnabled = true;
+            this.cmbCOM.Location = new System.Drawing.Point(170, 134);
+            this.cmbCOM.Name = "cmbCOM";
+            this.cmbCOM.Size = new System.Drawing.Size(85, 21);
+            this.cmbCOM.TabIndex = 13;
+            this.cmbCOM.SelectedIndexChanged += new System.EventHandler(this.cmbCOM_SelectedIndexChanged);
+            // 
+            // tmrRefresh
+            // 
+            this.tmrRefresh.Interval = 15000;
+            this.tmrRefresh.Tick += new System.EventHandler(this.tmrRefresh_Tick);
+            // 
+            // MainWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(276, 369);
+            this.Controls.Add(this.cmbCOM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHumidity);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblCO2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.labelHumid);
+            this.Controls.Add(this.labelTemp);
+            this.Controls.Add(this.labelCO2);
+            this.Controls.Add(this.lblPartyMode);
+            this.Controls.Add(this.lblSensorValue);
+            this.Controls.Add(this.labelPartyMode);
+            this.Controls.Add(this.labelSensorValue);
+            this.Name = "MainWindow";
+            this.Text = "Party Light GUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSensorValue;
+        private System.Windows.Forms.Label labelPartyMode;
+        private System.Windows.Forms.Label lblSensorValue;
+        private System.Windows.Forms.Label lblPartyMode;
+        private System.Windows.Forms.Timer tmrMotion;
+        private System.Windows.Forms.Timer tmrTick;
+        private System.Windows.Forms.Label labelCO2;
+        private System.Windows.Forms.Label labelTemp;
+        private System.Windows.Forms.Label labelHumid;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label lblCO2;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblHumidity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCOM;
+        private System.Windows.Forms.Timer tmrRefresh;
+    }
+}
+
---
+++ PartyLights/PartyLights/MainWindow.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+using Phidgets;
+using Phidgets.Events;
+
+
+namespace PartyLights
+{
+    public partial class MainWindow : Form
+    {
+        private InterfaceKit kit = new InterfaceKit();
+        private SerialPort serial = new SerialPort();
+        private bool isParty = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // ================ Main Function ====================
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                kit.Attach += new AttachEventHandler(kit_Attach);
+                kit.Detach += new DetachEventHandler(kit_Detach);
+                kit.SensorChange += new SensorChangeEventHandler(kit_SensorChange);
+                kit.open("192.168.0.22", 5001);
+                //kit.open("phidgetsbc");
+            }
+            catch (PhidgetException ex)
+            {
+                MessageBox.Show(ex.Description.ToString());
+            }
+
+            foreach (String str in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                cmbCOM.Items.Add(str);
+            }
+
+
+        }
+
+        private void MainWindow_FormClosing(object sender, EventArgs e)
+        {
+
+            kit.Attach -= new AttachEventHandler(kit_Attach);
+            kit.Detach -= new DetachEventHandler(kit_Detach);
+            kit.SensorChange -= new SensorChangeEventHandler(kit_SensorChange);
+            Application.DoEvents();
+            kit.close();
+            kit = null;
+        }
+
+        // ============== Event Subscribers ==================
+
+        private void kit_Attach(object sender, AttachEventArgs e)
+        {
+            kit.sensors[0].Sensitivity = 150;
+            kit.sensors[0].DataRate = 32;
+            kit.outputs[0] = false;
+            kit.outputs[1] = false;
+            kit.outputs[2] = false;
+        }
+
+        private void kit_Detach(object sender, DetachEventArgs e)
+        {
+            kit.outputs[0] = false;
+            kit.outputs[1] = false;
+            kit.outputs[2] = false;
+        }
+
+        private void kit_SensorChange(object sender, SensorChangeEventArgs e)
+        {
+            if (e.Index == 0)
+            {
+                lblSensorValue.Text = e.Value.ToString();
+
+                if (e.Value < 400 || e.Value > 600)
+                {
+                    isParty = true;
+                    tmrMotion.Enabled = true;
+                }
+            }
+
+            lblPartyMode.Text = (isParty) ? "ON" : "OFF";
+        }
+
+        private void tmrMotion_Tick(object sender, EventArgs e)
+        {
+            
+            if (kit != null)
+            {
+                kit.outputs[0] = false;
+                kit.outputs[1] = false;
+                kit.outputs[2] = false;
+                isParty = false;
+            }
+            
+        }
+
+        static void printError(int number, String description)
+        {
+            Console.WriteLine("Error on function call: {0} - {1}!", number, description);
+
+        }
+
+        private void tmrTick_Tick(object sender, EventArgs e)
+        {
+            if (isParty)
+            {
+                kit.outputs[0] = kit.outputs[0] ? false : true;
+                kit.outputs[1] = kit.outputs[0];
+                kit.outputs[2] = kit.outputs[1];
+                tmrTick.Enabled = true;
+            }
+
+        }
+
+        private void cmbCOM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            initiateRead(serial);
+            //readSensorValues(serial);
+            //tmrRefresh.Enabled = true;
+        }
+
+        private void tmrRefresh_Tick(object sender, EventArgs e)
+        {
+            readSensorValues(serial);
+            tmrRefresh.Enabled = true;
+        }
+
+        // ========= Regular Functions ============
+
+        public void initiateRead(SerialPort s)
+        {
+            if (cmbCOM.SelectedItem == null || s.IsOpen == true)
+            {
+                return;
+            }
+            try
+            {
+                s.PortName = (String)cmbCOM.SelectedItem;
+                s.Open();
+                if (s.IsOpen == true)
+                {
+                    lblCO2.Text = "andrew";
+                }
+                /*
+                Byte[] Cmd1 = new Byte[] { 0xFE, 0x41, 0x00, 0x60, 0x01, 0x30 };
+                addCheckSum(ref Cmd1);
+
+                while (true)
+                {
+                    s.DiscardInBuffer();
+                    s.Write(Cmd1, 0, Cmd1.Length);
+                    DateTime timeOut = DateTime.Now;
+                    while (s.BytesToRead < 4)
+                    {
+                        TimeSpan timePeriod = DateTime.Now - timeOut;
+                        if (timePeriod > TimeSpan.FromMilliseconds(90))
+                        {
+                            break;
+                        }
+                        System.Threading.Thread.Sleep(5);
+                    }
+
+                    if (s.BytesToRead >= 4)
+                    {
+                        Byte[] theResult = new Byte[4];
+                        s.Read(theResult, 0, 4);
+                        if (validatePacket(theResult))
+                        {
+                            break;
+                        }
+                    }
+
+                }
+                 */
+                s.Close();
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void readSensorValues(SerialPort s)
+        {
+            if (cmbCOM.SelectedItem == null || s.IsOpen == true)
+            {
+                return;
+            }
+
+            s.PortName = (String)cmbCOM.SelectedItem;
+            s.Open();
+
+            /* READ CO2 */
+            Byte[] Cmd1 = new Byte[] { 0xFE, 0x44, 0x00, 0x08, 0x02 };
+            addCheckSum(ref Cmd1);
+            while (true)
+            {
+                s.DiscardInBuffer();
+                s.Write(Cmd1, 0, Cmd1.Length);
+                DateTime timeOut = DateTime.Now;
+                while (s.BytesToRead < 7)
+                {
+                    TimeSpan timePeriod = DateTime.Now - timeOut;
+                    if (timePeriod > TimeSpan.FromMilliseconds(90))
+                    {
+                        break;
+                    }
+                    System.Threading.Thread.Sleep(5);
+                }
+                if (s.BytesToRead >= 7)
+                {
+                    Byte[] theResult = new Byte[7];
+                    s.Read(theResult, 0, 7);
+                    if (validatePacket(theResult))
+                    {
+                        lblCO2.Text = Convert.ToString(getInteger(theResult, 3));
+                        break;
+                    }
+                }
+            }
+
+            /* READ TEMP */
+            Byte[] Cmd2 = new Byte[] { 0xFE, 0x44, 0x00, 0x12, 0x02 };
+            addCheckSum(ref Cmd2);
+            while (true)
+            {
+                s.DiscardInBuffer();
+                s.Write(Cmd2, 0, Cmd2.Length);
+                DateTime timeOut = DateTime.Now;
+                while (s.BytesToRead < 7)
+                {
+                    TimeSpan timePeriod = DateTime.Now - timeOut;
+                    if (timePeriod > TimeSpan.FromMilliseconds(90))
+                    {
+                        break;
+                    }
+                    System.Threading.Thread.Sleep(5);
+                }
+                if (s.BytesToRead >= 7)
+                {
+                    Byte[] theResult = new Byte[7];
+                    s.Read(theResult, 0, 7);
+                    if (validatePacket(theResult))
+                    {
+                        lblTemp.Text = Convert.ToString(getInteger(theResult, 3));
+                        break;
+                    }
+                }
+            }
+
+            /* READ HUMIDITY */
+            Byte[] Cmd3 = new Byte[] { 0xFE, 0x44, 0x00, 0x14, 0x02 };
+            addCheckSum(ref Cmd3);
+            while (true)
+            {
+                s.DiscardInBuffer();
+                s.Write(Cmd3, 0, Cmd3.Length);
+                DateTime timeOut = DateTime.Now;
+                while (s.BytesToRead < 7)
+                {
+                    TimeSpan timePeriod = DateTime.Now - timeOut;
+                    if (timePeriod > TimeSpan.FromMilliseconds(90))
+                    {
+                        break;
+                    }
+                    System.Threading.Thread.Sleep(5);
+                }
+                if (s.BytesToRead >= 7)
+                {
+                    Byte[] theResult = new Byte[7];
+                    s.Read(theResult, 0, 7);
+                    if (validatePacket(theResult))
+                    {
+                        lblHumidity.Text = Convert.ToString(getInteger(theResult, 3));
+                        break;
+                    }
+                }
+            }
+            s.Close();
+
+        }
+
+        public short getInteger(Byte[] bytes, int offset)
+        {
+            Byte[] tempArray = { bytes[offset + 1], bytes[offset] };
+            return BitConverter.ToInt16(tempArray, 0);
+        }
+
+        public long CRC16(Byte[] MB_array, int mNum)
+        {
+            uint temp = 0;
+            uint CRC = 65535;
+            for (int i = 0; i < mNum; i++)
+            {
+                temp = CRC;
+                CRC = temp ^ MB_array[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((CRC & 1) > 0)
+                    {
+                        CRC = ((CRC >> 1) ^ 40961); //1021
+                    }
+                    else
+                    {
+                        CRC = (CRC >> 1); //And 65535
+                    }
+                }
+            }
+
+            return CRC & 65535;
+        }
+
+        public Boolean validatePacket(Byte[] packet)
+        {
+            int CRC = (int)CRC16(packet, packet.Length - 2);
+            int crcCompare = BitConverter.ToUInt16(packet, packet.Length - 2);
+            if (CRC != crcCompare)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void addCheckSum(ref Byte[] packet)
+        {
+            Byte[] CRC = BitConverter.GetBytes((short)(CRC16(packet, packet.Length)));
+            Byte[] outputArray = new Byte[packet.Length + 2];
+            packet.CopyTo(outputArray, 0);
+            CRC.CopyTo(outputArray, packet.Length);
+            packet = outputArray;
+        }
+    }       
+}
